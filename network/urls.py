592a27468efc81f881b17662f5512f7ad8cbf51d
Michail Melonas <michail@explore-ai.net>
2020-09-20 10:21:25 +0200

    changes

--- network/urls.py
+++ network/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('logout', views.logout_view, name='logout'),
     path('register', views.register, name='register'),
     path('api/create-post', views.create_post, name='create-post'),
-    path('api/adjust-emotion/<int:sentiment>/<int:post_id>', views.adjust_emotion)
+    path('api/adjust-emotion/<int:sentiment>/<int:post_id>', views.adjust_emotion),
+    path('api/get-posts/<str:filter_by>', views.get_posts),
+    path('api/get-post/<int:post_id>', views.get_post),
+    path('api/delete-post/<int:post_id>', views.delete_post),
+    path('api/update-post/<int:post_id>', views.update_post)
 ]
